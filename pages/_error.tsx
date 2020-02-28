b1c3e7e86e3d087f2c7a67af7c55815103be7745
parkoon <jh.park@qualson.com>
2020-02-28 17:27:27 +0900

    [기능] custom error 페이지 추가

---
+++ pages/_error.tsx
@@ -0,0 +1,25 @@
+import { ErrorProps } from 'next/error'
+
+function Error({ statusCode }: ErrorProps) {
+  const getResponse = (code: number) => {
+    switch (code) {
+      case 404:
+        return <h1>404 error</h1>
+
+      case 500:
+        return <p>An error occurred on the server</p>
+
+      default:
+        return <p>An error occurred on client</p>
+    }
+  }
+
+  return <>{getResponse(statusCode)}</>
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : null
+  return { statusCode }
+}
+
+export default Error
